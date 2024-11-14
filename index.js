0886706d4516c66c13ede811ad7d498c6edf4e22
KellynMoodley <KMoodley@datacentrix.co.za>
2024-11-14 13:07:09 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files (like index.html) from the 'public' folder
-app.use(express.static(path.join(__dirname, 'public')));
+// Serve the index.html directly from the root directory
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Start the server
 app.listen(process.env.PORT || 3000, () => {
