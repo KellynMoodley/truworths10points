44e6db5d245bdc6d8e09bed479b8765b6d7bbfad
KellynMoodley <KMoodley@datacentrix.co.za>
2024-11-21 09:39:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cors());
+app.use(express.json());
+
 // Watson Speech to Text credentials
 const watsonSpeechToTextUrl = 'https://api.us-south.speech-to-text.watson.cloud.ibm.com/instances/d0fa1cd2-f3b4-4ff0-9888-196375565a8f';
 const watsonSpeechToTextApiKey = 'ig_BusJMZMAOYfhcRJ-PtAf4PgjzSIMebGjszzJZ9RIj';
 
 const ACCESS_TOKEN = 'pat-na1-bc9ea2a9-e8e6-42a1-99ed-43276eadb3ac';
 
-app.use(cors());
-app.use(express.json());
 
 // Store calls and conversations in memory
 app.locals.currentCall = null;
