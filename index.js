5e23a332e10a66ed62fba1be04e901d647c398d5
KellynMoodley <KMoodley@datacentrix.co.za>
2024-11-25 21:41:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -152,7 +152,8 @@ app.post('/process-speech', async (req, res) => {
 
     let botResponse = 'Thank you for your message. Goodbye!';
 
-    if (speechResult.toLowerCase().includes('option one')) {
+    if (speechResult.toLowerCase().includes('option 1') || 
+    speechResult.toLowerCase().includes('option one')) {
   const response = new twiml.VoiceResponse();
   response.say('Please provide your first name and last name.');
   
