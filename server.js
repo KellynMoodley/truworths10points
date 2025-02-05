1c0143d15de5e730a9fb3527ea2301d47d04d42f
KellynMoodley <KMoodley@datacentrix.co.za>
2025-02-05 17:14:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // Add this line
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
