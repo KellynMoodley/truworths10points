0bdb82593206bdc5e2860b4acb0f93997c1b308a
KellynMoodley <KMoodley@datacentrix.co.za>
2025-02-13 12:41:51 +0200

    Update server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/', (req, res) => {
 app.get('/fetch-summary', async (req, res) => {
     const accountNumber = req.query.account;
     try {
-        const response = await axios.get(`https://kkarodia.app.n8n.cloud/webhook/fc65bc6d-e420-482e-ba3b-cea430d402ff?account=${encodeURIComponent(accountNumber)}`);
+        const response = await axios.get(`https://kkarodia.app.n8n.cloud/webhook/078d2b54-2e9a-457e-9f4f-05bda28e385a?account=${encodeURIComponent(accountNumber)}`);
          res.json(response.data);
     } catch (error) {
         res.status(404).json({ error: 'No data found' });
